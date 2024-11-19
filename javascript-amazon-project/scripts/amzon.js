e01954e1b3798472518e8e441573e231540d3199
Nonamechick1 <otajonovxasan350@gmail.com>
2024-11-19 21:09:02 +0500

    Make the cart quantity interactive

--- javascript-amazon-project/scripts/amzon.js
+++ javascript-amazon-project/scripts/amzon.js
@@ -112,6 +112,17 @@ console.log(productsHTML);
             quantity: 1
           });
         }
-        console.log(cart);
+
+        let cartQuantity = 0;
+
+        cart.forEach((item)=>{
+          cartQuantity+=item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+         .innerHTML = cartQuantity;
+
+        // console.log(cartQuantity);
+        // console.log(cart);
     });
  });
