984812981bf4768932027c12859e9640cbb7f41b
Nonamechick <otajonovxasan350@gmail.com>
2024-11-23 19:02:59 +0530

    Move addToCart to cart.js

--- javascript-amazon-project/scripts/amzon.js
+++ javascript-amazon-project/scripts/amzon.js
@@ -1,5 +1,7 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import {products} from "../data/products.js";
+
+
 
 // const products = [{
 //     image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
@@ -94,36 +96,24 @@ console.log(productsHTML);
  document.querySelector('.js-products-grid')
  .innerHTML=productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem)=>{
+    cartQuantity+=cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity')
+   .innerHTML = cartQuantity;
+
+}
+
  document.querySelectorAll('.js-add-to-cart')
  .forEach((button)=>{
     button.addEventListener('click', ()=>{
         const productId = button.dataset.productId;
-
-        let matchingItem;
-
-        cart.forEach((item)=> {
-          if (productId === item.productId) {
-            matchingItem = item;
-          }
-        });
-
-        if (matchingItem ) {
-          matchingItem.quantity+=1;
-        } else {
-          cart.push({
-            productId: productId,
-            quantity: 1
-          });
-        }
-
-        let cartQuantity = 0;
-
-        cart.forEach((item)=>{
-          cartQuantity+=item.quantity;
-        });
-
-        document.querySelector('.js-cart-quantity')
-         .innerHTML = cartQuantity;
+        addToCart(productId);
+        updateCartQuantity();
 
         // console.log(cartQuantity);
         // console.log(cart);
