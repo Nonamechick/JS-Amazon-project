b8cc452fb57b6913e01323aa6f89a3c4a29c20c7
Nonamechick <otajonovxasan350@gmail.com>
2024-11-24 13:49:15 +0530

    Save changes

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,6 +1,7 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart,calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
+
 
 
 let cartSummaryHTML = '';
@@ -113,6 +114,20 @@ document.querySelectorAll('.js-delete-link')
             );
             // console.log(container);
             container.remove();
+
+            updateCartQuantity();
         });
     });
 // console.log(cartSummaryHTML);
+
+
+
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector('.js-return-to-home-link')
+  .innerHTML = `${cartQuantity} items`;
+}
+updateCartQuantity();
+
+
